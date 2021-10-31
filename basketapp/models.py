2663ecf209d790b28da856b2a05dbbf794aa646d
VitaliiLoboda <vitalii.loboda@gmail.com>
2021-10-31 16:11:06 +0200

    lesson_6

--- basketapp/models.py
+++ basketapp/models.py
@@ -11,3 +11,16 @@ class Basket(models.Model):
 
     add_datetime = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def product_cost(self):
+        return self.quantity * self.product.price
+
+    @property
+    def total_quantity(self):
+        _items = Basket.objects.filter(user=self.user)
+        return sum(list(map(lambda x: x.quantity, _items)))
+
+    @property
+    def total_cost(self):
+        _items = Basket.objects.filter(user=self.user)
+        return sum(list(map(lambda x: x.product_cost, _items)))
