fb27e1a87bee4c8ded10d1e5768cebedbf69b43a
VitaliiLoboda <vitalii.loboda@gmail.com>
2021-11-12 12:13:35 +0200

    lesson_7

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.contrib.auth.decorators import user_passes_test
-from django.urls import reverse
-from adminapp.forms import ShopUserAdminEditForm
+from django.utils.decorators import method_decorator
+from django.urls import reverse, reverse_lazy
+from adminapp.forms import ShopUserAdminEditForm, ProductEditForm
 from authapp.forms import ShopUserRegisterForm
 from authapp.models import ShopUser
 from mainapp.models import ProducCategory, Product
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -24,12 +26,26 @@ def user_create(request):
     return render(request, 'adminapp/user_form.html', context)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def users(request):
-    context = {
-        'object_list': ShopUser.objects.all().order_by('-is_active')
-    }
-    return render(request, 'adminapp/users.html', context)
+# @user_passes_test(lambda u: u.is_superuser)
+# def users(request):
+#     context = {
+#         'object_list': ShopUser.objects.all().order_by('-is_active')
+#     }
+#     return render(request, 'adminapp/users.html', context)
+
+class AccessMixin:
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+
+class UserListView(AccessMixin, ListView):
+    model = ShopUser
+    template_name = 'adminapp/users.html'
+
+    # @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    # def dispatch(self, request, *args, **kwargs):
+    #     return super().dispatch(request, *args, **kwargs)
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -100,42 +116,97 @@ def category_delete(request):
     return render(request, '', context)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def product_create(request):
-    context = {
-
-    }
-    return render(request, '', context)
-
-
-@user_passes_test(lambda u: u.is_superuser)
-def products(request, pk):
-    context = {
-        'category': get_object_or_404(ProducCategory, pk=pk),
-        'object_list': Product.objects.filter(category__pk=pk).order_by('-is_active')
-    }
-    return render(request, 'adminapp/products.html', context)
+# @user_passes_test(lambda u: u.is_superuser)
+# def product_create(request):
+#     context = {
+#
+#     }
+#     return render(request, '', context)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def product_update(request):
-    context = {
+class ProductCreateView(AccessMixin, CreateView):
+    model = Product
+    template_name = 'adminapp/product_form.html'
+    form_class = ProductEditForm
 
-    }
-    return render(request, '', context)
+    # fields = '__all__'
+    # success_url = reverse_lazy('adminapp:category_list')
 
-
-@user_passes_test(lambda u: u.is_superuser)
-def product_detail(request):
-    context = {
-
-    }
-    return render(request, '', context)
+    def get_success_url(self):
+        return reverse('adminapp:product_list', args=[self.kwargs['pk']])
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def product_delete(request):
-    context = {
+# @user_passes_test(lambda u: u.is_superuser)
+# def products(request, pk):
+#     context = {
+#         'category': get_object_or_404(ProducCategory, pk=pk),
+#         'object_list': Product.objects.filter(category__pk=pk).order_by('-is_active')
+#     }
+#     return render(request, 'adminapp/products.html', context)
 
-    }
-    return render(request, '', context)
+
+class ProductsListView(AccessMixin, ListView):
+    model = Product
+    template_name = 'adminapp/products.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        context_data['category'] = get_object_or_404(ProducCategory, pk=self.kwargs.get('pk'))
+        return context_data
+
+    def get_queryset(self):
+        return Product.objects.filter(category__pk=self.kwargs.get('pk'))
+
+
+# @user_passes_test(lambda u: u.is_superuser)
+# def product_update(request):
+#     context = {
+#
+#     }
+#     return render(request, '', context)
+
+
+class ProductUpdateView(AccessMixin, UpdateView):
+    model = Product
+    template_name = 'adminapp/product_form.html'
+    form_class = ProductEditForm
+
+    def get_success_url(self):
+        product_item = Product.objects.get(pk=self.kwargs['pk'])
+        return reverse('adminapp:product_list', args=[product_item.category_id])
+
+
+# @user_passes_test(lambda u: u.is_superuser)
+# def product_detail(request, pk):
+#     context = {
+#
+#     }
+#     return render(request, '', context)
+
+class ProductDetailView(AccessMixin, DetailView):
+    model = Product
+    template_name = 'adminapp/product_detail.html'
+
+
+# @user_passes_test(lambda u: u.is_superuser)
+# def product_delete(request):
+#     context = {
+#
+#     }
+#     return render(request, '', context)
+
+class ProductDeleteView(AccessMixin, DeleteView):
+    model = Product
+    template_name = 'adminapp/product_delete.html'
+
+    def get_success_url(self):
+        product_item = Product.objects.get(pk=self.kwargs['pk'])
+        return reverse('adminapp:product_list', args=[product_item.category_id])
+
+    # def delete(self, request, *args, **kwargs):
+    #     if self.object.is_active:
+    #         self.object.is_active = False
+    #     else:
+    #         self.object.is_active = True
+    #     self.object.save()
+    #     return HttpResponseRedirect(reverse('adminapp:product_list', args=[self.object.category_id]))
--- mainapp/models.py
+++ mainapp/models.py
@@ -28,3 +28,9 @@ class Product(models.Model):
     def __str__(self):
         return f'{self.name} ({self.category.name})'
 
+    def delete(self):
+        if self.is_active:
+            self.is_active = False
+        else:
+            self.is_active = True
+        self.save()
